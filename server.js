492651a35f5e12df3a0b2efa12d70a4980b12610
ANJALI <aljaiswal173@gmail.com>
2024-12-16 02:42:46 +0530

    add package.json

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+//const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+//connectDB();
+
+// Initialize Express App
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Basic Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
