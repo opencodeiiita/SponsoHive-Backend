e59e92e07ff937d46761d37daa402a1c6e1e1d8a
Ayush Patel <148226375+ayush-patel1@users.noreply.github.com>
2024-12-17 15:31:28 +0000

    update

--- models/EmailList.js
+++ models/EmailList.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const emailListSchema = new mongoose.Schema({
   name: { type: String, required: true },
   tags: { type: String, default: null },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  // Reference to User
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 });
 
 const EmailList = mongoose.model('EmailList', emailListSchema);
+
 module.exports = EmailList;
