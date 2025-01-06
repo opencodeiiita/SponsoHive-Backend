62df2255276e8e3bf335c4b8df93b32466a38df2
Rajbir Singh <rajbir7528@gmail.com>
2025-01-06 17:33:23 +0530

    Bounce complaint management

--- models/Recipient.js
+++ models/Recipient.js
@@ -5,6 +5,9 @@ const recipientSchema = new mongoose.Schema({
   lastName: { type: String },
   email: { type: String, required: true },
   orderId: { type: String },
+  isActive: { type: Boolean, default: true },
+  bounceCount: { type: Number, default: 0 },
+  complaintCount: { type: Number, default: 0 },
   // Add other fields as needed
 });
 
--- services/emailService.js
+++ services/emailService.js
@@ -1,5 +1,9 @@
 
 const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.EMAIL_API_KEY);
+const EmailReply = require("../models/emailReply.js");
+const Recipient = require("../models/Recipient.js");
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -23,6 +27,36 @@ const sendFollowUpEmail = async (email, campaign) => {
   } catch (error) {
     console.error(`Error sending follow-up email: ${error.message}`);
   }
+};
+
+// send mail using sendgrid
+
+const sendEmail = async (recipientId, campaignId, subject) => {
+
+  const emailReply = await EmailReply.create({recipientId , campaignId , messageId:"efbhcdj"}); // add unique message id for each email
+  const recipient = await Recipient.create({firstName:"abc" , email:recipientId}); // recipient name
+
+  try {
+    await sgMail.send({
+        personalizations: [
+            {
+                to: [{ email: recipientId }],
+                custom_args: {
+                    message_id: 'wdhdbha' //add unique message id for handling event webhook
+                }
+            }
+        ],
+        from: campaignId,
+        subject: 'Email from SponsoHive',
+        text: 'This is a test email to check if emails are being sent correctly.', // Use `text` instead of `body`
+        html: '<p>This is a test email to check if emails are being sent correctly.</p>' // Optionally add HTML content
+    });
+
+    res.status(200).send('Email sent successfully.');
+} catch (error) {
+    console.error('Failed to send email:', error.response.body.errors || error.message);
+    res.status(500).send('Failed to send email. Please try again later.');
+}
 };
 
 module.exports = { sendFollowUpEmail };
